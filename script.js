5c2a2b87c4015ad884a0ef72b6948487abdac3ce
IgorDobryansky <dobryansky.igor.m@gmail.com>
2025-11-23 21:55:48 +0200

    Fix clock numbers orientation

--- script.js
+++ script.js
@@ -17,18 +17,9 @@ function setClock() {
 
 function setRotation(id, rotationRatio) {
   const element = document.getElementById(id);
-  // Fix for the jump when going from 359 to 0 degrees
-  // We can just keep increasing degrees, but for simplicity in this version
-  // we will handle the basic rotation.
-  // To make it robust against the 360->0 jump animation glitch, we would typically remove transition
-  // but with the current CSS transition it might be slightly noticeable once a minute.
-  // A common fix is to not use 0-360 but continuous accumulation, but let's stick to simple first.
-
   element.style.transform = `translateX(-50%) rotate(${rotationRatio}deg)`;
 }
 
-// Update every second
 setInterval(setClock, 1000);
 
-// Initialize immediately
-setClock();
+// setClock();
